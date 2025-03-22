6632c967f777772478c64e0bc589d7c4b6f432ba
Canhnv18 <ninhvancanh38@gmal.com>
2025-03-22 16:06:14 +0700

    update trang detail product

---
+++ Controller/DetailController.php
@@ -0,0 +1,23 @@
+<?php
+require_once './Model/Product.php';
+require_once './Model/Brand.php';
+
+class DetailController
+{
+    public function Detail()
+    {
+
+        $product = new Product;
+        $id = isset($_GET['id']) ? $_GET['id'] : 1;
+
+        $brand_model = new Brand();
+        $all_brands = $brand_model->getAllBrand();
+
+        $detail_product = $product->getProductById($id);
+        $similar_product = $product->getProductFromBrand($detail_product['brand_id']);
+
+        include('./Views/Layout/Header.php');
+        include('./Views/detailProduct.php');
+        include('./Views/Layout/Footer.php');
+    }
+}

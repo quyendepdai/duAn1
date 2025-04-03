ff8ff5b64e9ca57a0ef960b9599e75566c274785
QuyenDz <qh10072504@gmail.com>
2025-04-03 14:12:34 +0700

    quyen_update_4/3

---
+++ Controller/LogoutController.php
@@ -0,0 +1,12 @@
+<?php
+class LogoutController
+{
+    public function logout()
+    {
+        session_start();
+        session_unset();
+        session_destroy();
+        header("Location: index.php?route=login");
+        exit();
+    }
+}

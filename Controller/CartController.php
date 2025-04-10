5d21c1289e92ad7f68294a4ca4064beaf48dfb6c
Canhnv18 <ninhvancanh38@gmal.com>
2025-04-10 16:33:52 +0700

    canh update 10/4

--- Controller/CartController.php
+++ Controller/CartController.php
@@ -33,13 +33,13 @@ class CartController
     public function Cart()
     {
 
-        
-         require_once './Model/Cart.php';
 
-        if(isset($_SESSION['user_id'])){
+        require_once './Model/Cart.php';
+
+        if (isset($_SESSION['user_id'])) {
             $user_id = $_SESSION['user_id'];
             $cartModel = new Cart();
-            $carts = $cartModel->getInfoProductByUser($user_id); 
+            $carts = $cartModel->getInfoProductByUser($user_id);
             $countCartByUser = $cartModel->countByUserId($user_id);
         }
 
@@ -47,7 +47,7 @@ class CartController
         $brand_model = new Brand();
         $all_brands = $brand_model->getAll();
 
-        include('./Views/Layout/Header.php');
+        require_once('./Views/Layout/Header.php');
         include('./Views/Cart.php');
         include('./Views/Layout/Footer.php');
     }
@@ -64,33 +64,34 @@ class CartController
         $cardByUserId = $cardModel->getCartByUserId($user_id);
 
         $isSet = false;
-        echo($product_id.'<br>');
-        if(!empty($cardByUserId)){
-           foreach ($cardByUserId as $value) {
-            echo($value['product_id'].'<br>');
+        echo ($product_id . '<br>');
+        if (!empty($cardByUserId)) {
+            foreach ($cardByUserId as $value) {
+                echo ($value['product_id'] . '<br>');
 
-                if($value['product_id'] == $product_id){
-                    echo('true');
+                if ($value['product_id'] == $product_id) {
+                    echo ('true');
                     $value['quantity'] += 1;
-                    $addToCart = $cardModel->updateQuantityInCart($value['cart_id'],$product_id,$value['quantity']);
+                    $addToCart = $cardModel->updateQuantityInCart($value['cart_id'], $product_id, $value['quantity']);
                     $isSet = true;
                     break;
                 }
-           }
+            }
         }
-        if(!$isSet){
+        if (!$isSet) {
             $quantity = 1;
             $date = date("Y-m-d H:i:s");
-            $addToCart = $cardModel->addToCart($user_id,$product_id,$quantity,$date);
-        }  
+            $addToCart = $cardModel->addToCart($user_id, $product_id, $quantity, $date);
+        }
 
-        header('Location: index.php' );
+        header('Location: index.php');
         exit();
     }
 
-    public function addNewOrder() {
+    public function addNewOrder()
+    {
 
-         if (isset($_POST)){
+        if (isset($_POST)) {
             print_r($_POST);
             $name = $_POST['name'];
             $phone = $_POST['phone'];
@@ -105,33 +106,33 @@ class CartController
             // lấy các bản ghi trong cart
             $cardModel = new Cart();
             $cardByUserId = $cardModel->getInfoProductByUser($user_id);
-            echo('<pre>');
+            echo ('<pre>');
             print_r($cardByUserId);
-            echo('</pre>');
+            echo ('</pre>');
             $orderModel = new Order();
 
             //insert vào bảng Order
-            $newOrderId = $orderModel->addIntoOrder($user_id,$date,$totalMoney);
+            $newOrderId = $orderModel->addIntoOrder($user_id, $date, $totalMoney);
 
             //insert vào bảng OrderDetail và xóa cart
-            foreach($cardByUserId as $key => $value){
-                $orderModel->addIntoOrderDetail($newOrderId,$value['product_id'],$value['quantity'],$value['price']);
-                $cardModel->deleteCartItem($value['product_id'],$user_id);
+            foreach ($cardByUserId as $key => $value) {
+                $orderModel->addIntoOrderDetail($newOrderId, $value['product_id'], $value['quantity'], $value['price']);
+                $cardModel->deleteCartItem($value['product_id'], $user_id);
             }
 
             //insert vào bảng Payments
-            $insertPayment = $orderModel->addIntoPayment($newOrderId,$paymentMethod);
-            
-            //insert vào bảng shipping
-            $insertShipping = $orderModel->addIntoShipping($newOrderId,$address,$phone,$name,$note);
+            $insertPayment = $orderModel->addIntoPayment($newOrderId, $paymentMethod);
 
-          
+            //insert vào bảng shipping
+            $insertShipping = $orderModel->addIntoShipping($newOrderId, $address, $phone, $name, $note);
+
+
 
 
             $_SESSION['success'] = 'Đặt hàng thành công';
             header('Location: index.php?route=order');
-         }
-        
+        }
+
         // // Chuyển về trang chủ
         // header('Location: index.php');
         // exit();
@@ -145,24 +146,25 @@ class CartController
 
         $cartModel = new Cart();
 
-        $isDelete = $cartModel->deleteCartItem($product_id,$user_id);
+        $isDelete = $cartModel->deleteCartItem($product_id, $user_id);
 
-      
 
-       if( !$isDelete ){
-           $_SESSION['error'] = "Xóa thất bại! Vui lòng thử lại sau.";
-       }
+
+        if (!$isDelete) {
+            $_SESSION['error'] = "Xóa thất bại! Vui lòng thử lại sau.";
+        }
         header('Location: index.php?route=cart');
         exit();
     }
 
-    public function Order(){
-           require_once './Model/Cart.php';
+    public function Order()
+    {
+        require_once './Model/Cart.php';
 
-        if(isset($_SESSION['user_id'])){
+        if (isset($_SESSION['user_id'])) {
             $user_id = $_SESSION['user_id'];
             $cartModel = new Cart();
-            $carts = $cartModel->getInfoProductByUser($user_id); 
+            $carts = $cartModel->getInfoProductByUser($user_id);
             $countCartByUser = $cartModel->countByUserId($user_id);
         }
 

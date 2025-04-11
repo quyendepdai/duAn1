b7b6f98da8055ab8e5160416980eb6010a0f7d43
longvu-bit <longvussw35@gmail.com>
2025-04-11 07:35:51 +0700

    long update

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -7,14 +7,24 @@ class HomeController
         require_once './Model/Category.php';
         require_once './Model/Brand.php';
 
+        require_once './Model/Cart.php';
+
+        if(isset($_SESSION['user_id'])){
+            $user_id = $_SESSION['user_id'];
+            $cartModel = new Cart();
+            $countCartByUser = $cartModel->countByUserId($user_id);
+        }
+        
+
+       
+
         $category_model = new Category();
         $all_category = $category_model->getAllCategory();
 
         $brand_model = new Brand();
-        $all_brands = $brand_model->getAllBrand();
+        $all_brands = $brand_model->getAll();
 
         $product_model = new Product();
-        $all_products = $product_model->getAllProduct();
 
         $limit = 8;
         $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
@@ -28,7 +38,7 @@ class HomeController
 
 
 
-        // print_r($all_category)
+        // print_r($all_category);
         include('./Views/Layout/Header.php');
         include('./Views/Home.php');
         include('./Views/Layout/Footer.php');

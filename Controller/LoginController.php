cc59da8400ce68d4a25845a83bc832cab3e2820b
longvu-bit <longvussw35@gmail.com>
2025-04-11 07:36:17 +0700

    long update

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -8,7 +8,6 @@ class LoginController
     public function __construct()
     {
         $this->userModel = new UserModel();
-        
     }
 
     public function login()
@@ -24,14 +23,19 @@ class LoginController
                 $_SESSION['username'] = $user['Username'];
                 $_SESSION['role'] = $user['Role'];
 
-                // Kiểm tra và điều hướng dựa vào role
                 if ($user['Role'] === 'admin') {
-                    header("Location: index.php?route=admin/products"); // Điều hướng admin đến trang quản lý sản phẩm
-                    exit();
-                } else {
-                    header("Location: index.php?route=home"); // User thường về trang chủ
+                    header("Location: index.php?route=admin/products");
                     exit();
                 }
+                 else {
+                    header("Location: index.php?route=home");
+                    exit();
+                }
+            }
+            if (empty($username) || empty($password)) {
+                $_SESSION['error'] = 'Bạn chưa nhập tên tài khoản hoặc password';
+                header("Location: index.php?route=login");
+                exit();
             } else {
                 $_SESSION['error'] = "Tên đăng nhập hoặc mật khẩu không đúng";
             }
